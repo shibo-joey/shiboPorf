8898f1bce0887ca831007e3b4bf76fb7c045c625
Your Name <you@example.com>
2025-01-08 00:01:53 -0800

    fixed build issue

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Navigation from "./components/Navigation";
 import { useState } from "react";
 import Footer from "./components/Footer";
 import MainPage from "./components/MainPage";
-import { Helmet } from "react-helmet-async";
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -21,22 +20,6 @@ function App() {
   };
   return (
     <div style={{ background: theme === "light" ? "white" : "#0d1117" }}>
-      <Helmet>
-        <title>Shibo Yang - Senior Software Engineer</title>
-        <meta
-          name="description"
-          content="Shibo Yang - Senior Software Engineer specializing in web development, cloud computing, and modern frameworks like Next.js, React, and Node.js. Explore my portfolio and projects."
-        />
-        <meta
-          name="keywords"
-          content="Shibo Yang, software engineer, web developer, React, Next.js, AWS, Tencent Cloud, Node.js, JavaScript, TypeScript, portfolio, coding projects"
-        />
-        <meta name="author" content="Shibo Yang" />
-        <link
-          rel="canonical"
-          href="https://www.linkedin.com/in/shibo-yang-912134111"
-        />
-      </Helmet>
       <Navigation theme={theme} changeTheme={changeTheme} />
       <Switch>
         <Route
--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,37 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./i18n";
+import { HelmetProvider } from "react-helmet-async";
+import { Helmet } from "react-helmet-async";
+
+const helmetContext = {}; // Create an empty context
 
 const app = (
   <Router>
+    <Helmet>
+      <title>Shibo Yang - Senior Software Engineer</title>
+      <meta
+        name="description"
+        content="Shibo Yang - Software Engineer specializing in web development, cloud computing, and modern frameworks like Next.js, React, and Node.js. Explore my portfolio and projects."
+      />
+      <meta
+        name="keywords"
+        content="Shibo Yang, software engineer, web developer, React, Next.js, AWS, Tencent Cloud, Node.js, JavaScript, TypeScript, portfolio, coding projects"
+      />
+      <meta name="author" content="Shibo Yang" />
+      <link
+        rel="canonical"
+        href="https://www.linkedin.com/in/shibo-yang-912134111"
+      />
+    </Helmet>
     <App />
   </Router>
 );
 
-ReactDOM.render(app, document.getElementById("root"));
+ReactDOM.render(
+  <HelmetProvider context={helmetContext}>{app}</HelmetProvider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
